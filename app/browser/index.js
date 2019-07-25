2a751c0aa283a1ac323ea2a677a5811dc9544449
Ismael Martinez Ramos <ismael.martinez@postcodelottery.co.uk>
2019-07-25 11:56:19 +0100

    WIP

--- app/browser/index.js
+++ app/browser/index.js
@@ -10,7 +10,7 @@
 		require('./onlineOfflineListener')();
 	}
 	require('./zoom')();
-	
+
 	const iconPath = path.join(__dirname, '../assets/icons/icon-96x96.png');
 
 	new ActivityManager(ipcRenderer, iconPath).start();
@@ -19,38 +19,40 @@
 		pageTitleNotifications(ipcRenderer);
 	}
 
-	document.addEventListener(
-		'DOMContentLoaded',
-		() => {
-
-			setTimeout(() => {
-				if (angular) {
-					let injector = angular.element(document).injector();
-
-					if(injector) {
-						enableChromeVideoAudioMeetings(injector);
-						disablePromoteStuff(injector);
-						
-						injector.get('settingsService').settingsService.refreshSettings();
-						
-					}
-					 document.getElementById('screens-sources').innerHTML = "<div id='desktopCapturerGallery'><a href='testing'>testing</a>";	
-				}
-				// Future tests can be done in here...
-				// angular.element(document).injector().get('settingsService').appConfig.replyBoxFocusAfterNewMessage = true;
-				//last I look is enableIncomingVideoUnsupportedUfd groing from down to up.
-			}, 4000);
+	document.addEventListener('DOMContentLoaded',() => {modifyAngularSettingsWithTimeot();
 			// require('./desktopShare/chromeApi');
-
 		},
 	);
+
+	function modifyAngularSettingsWithTimeot() {
+		setTimeout(() => {
+			try {
+				let injector = angular.element(document).injector();
+
+				if(injector) {
+					enableChromeVideoAudioMeetings(injector);
+					disablePromoteStuff(injector);
+
+					injector.get('settingsService').settingsService.refreshSettings();
+				}
+				 document.getElementById('screens-sources').innerHTML = "<div id='desktopCapturerGallery'><a href='testing'>testing</a>";
+			} catch (error) {
+				if (error instanceof ReferenceError) {
+					modifyAngularSettingsWithTimeot();
+				}
+			}
+			// Future tests can be done in here...
+			// angular.element(document).injector().get('settingsService').appConfig.replyBoxFocusAfterNewMessage = true;
+			//last I look is enableIncomingVideoUnsupportedUfd groing from down to up.
+		}, 4000);
+	}
 
 	function enableChromeVideoAudioMeetings(injector) {
 		injector.get('callingSupportService').oneOnOneCallingEnabled = true;
 		injector.get('callingSupportService').isChromeMeetingSingleVideoEnabled = true;
 		injector.get('callingSupportService').isChromeVideoOneOnOneEnabled = true;
 		injector.get('callingSupportService').isChromeVideoMultipartyEnabled = true;
-		injector.get('callingSupportService').isWeb = false;
+		injector.get('callingSupportService').isDesktopApp = true;
 		injector.get('settingsService').appConfig.enableCallingChromeOneOnOne = true;
 		injector.get('settingsService').appConfig.callingEnableChromeMeetingSingleVideo = true;
 		injector.get('settingsService').appConfig.callingEnableChromeMultipartyVideo = true;
